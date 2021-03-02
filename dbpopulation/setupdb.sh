7c2a61edefc20a44920fef1ea4d0326f3ac4cec5
altanai <tara181989@gmail.com>
2021-03-02 08:21:30 -0800

    local commit

--- dbpopulation/setupdb.sh
+++ dbpopulation/setupdb.sh
@@ -2,3 +2,19 @@
 #
 # ./gradlew clean build
 
+# For single json file import
+#mongoimport -d databaseName -c collectionName --file myJsonFile.json -u username -p password --authenticationDatabase admin
+
+#with auth
+# -u username -p password --authenticationDatabase admin
+#mongoimport --host=localhost --port=27017 --db=address --collection=us --type=csv --file=us/*.csv --headerline
+
+cd us/wa
+
+for i in *.csv;
+do
+  echo ${i%.*};
+  mongoimport --host=localhost --port=27017 --db=address --collection=us_wa --drop --type=csv --file=$i --headerline --mode=upsert;
+done
+
+echo "end of import."
