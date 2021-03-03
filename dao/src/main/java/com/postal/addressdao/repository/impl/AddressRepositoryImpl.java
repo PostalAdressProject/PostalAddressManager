48beff71dde85af9014f8d5dbe58a4165e2e08a4
Shrividya Ballapadavu <shrividya94@gmail.com>
2021-03-02 16:58:38 -0800

    Fixed issue with mapping document to Address

--- dao/src/main/java/com/postal/addressdao/repository/impl/AddressRepositoryImpl.java
+++ dao/src/main/java/com/postal/addressdao/repository/impl/AddressRepositoryImpl.java
@@ -1,71 +1,72 @@
 package com.postal.addressdao.repository.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.client.model.Filters;
 import com.postal.addressdao.repository.AddressRepository;
+import com.postal.addressdao.utils.AddressCodeUtils;
 import com.postal.model.enums.Field;
 import com.postal.model.models.Address;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
 import org.bson.conversions.Bson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-
 @Component
 public class AddressRepositoryImpl implements AddressRepository {
 
-    @Autowired
-    private MongoTemplate mongoTemplate;
+	@Autowired
+	private MongoTemplate mongoTemplate;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+	@Autowired
+	private ObjectMapper objectMapper;
 
-    @Override
-    public List<Address> findAddressByCountry(final String countryName,
-                                              final Map<Field, String> fieldsMap)
-            throws IOException {
-        String collectionName = com.postal.address.utils.AddressCodeUtils.getCodeByCountry(countryName);
-        collectionName =
-                StringUtils.isBlank(collectionName) ? collectionName : collectionName.toLowerCase();
-        System.out.println("CountryCode:" + collectionName);
-        if (StringUtils.isBlank(collectionName)) {
-            throw new IllegalArgumentException("Country Name is empty or invalid");
-        }
-        final boolean isPresent = mongoTemplate.collectionExists(collectionName);
-        if (!isPresent) {
-            throw new IllegalArgumentException("Country Name is not supported");
-        }
-        final List<org.bson.Document> results = new ArrayList<>();
-        mongoTemplate.getCollection(collectionName)
-                .find(Filters.and(getFilters(fieldsMap))).into(results);
-        return getAddressFromDocument(results);
-    }
+	@Override
+	public List<Address> findAddressByCountry(final String countryName,
+		final Map<Field, String> fieldsMap)
+		throws IOException {
+		String collectionName = AddressCodeUtils.getCodeByCountry(countryName);
+		collectionName =
+			StringUtils.isBlank(collectionName) ? collectionName : collectionName.toLowerCase();
+		System.out.println("CountryCode:" + collectionName);
+		if (StringUtils.isBlank(collectionName)) {
+			throw new IllegalArgumentException("Country Name is empty or invalid");
+		}
+		final boolean isPresent = mongoTemplate.collectionExists(collectionName);
+		if (!isPresent) {
+			throw new IllegalArgumentException("Country Name is not supported");
+		}
+		final List<org.bson.Document> results = new ArrayList<>();
+		mongoTemplate.getCollection(collectionName)
+			.find(Filters.and(getFilters(fieldsMap))).into(results);
+		return getAddressFromDocument(results);
+	}
 
-    private Iterable<Bson> getFilters(final Map<Field, String> fieldsMap) {
-        final List<Bson> filters = new ArrayList<>();
-        final Iterator<Field> keyItr = fieldsMap.keySet().iterator();
-        while (keyItr.hasNext()) {
-            final Field field = keyItr.next();
-            filters.add(Filters.eq(field.toString(), fieldsMap.get(field)));
-        }
-        return filters;
-    }
+	private Iterable<Bson> getFilters(final Map<Field, String> fieldsMap) {
+		final List<Bson> filters = new ArrayList<>();
+		final Iterator<Field> keyItr = fieldsMap.keySet().iterator();
+		while (keyItr.hasNext()) {
+			final Field field = keyItr.next();
+			filters.add(Filters.eq(field.toString(), fieldsMap.get(field)));
+		}
+		return filters;
+	}
 
-    private List<Address> getAddressFromDocument(final List<org.bson.Document> documents)
-            throws IOException {
-        final String jsonDocuments = objectMapper.writeValueAsString(documents);
-        final TypeReference<List<Address>> typeRef
-                = new TypeReference<List<Address>>() {
-        };
+	private List<Address> getAddressFromDocument(final List<org.bson.Document> documents)
+		throws JsonProcessingException {
+		final String jsonDocuments = objectMapper.writeValueAsString(documents);
 
-        final List<Address> results = objectMapper.readValue(jsonDocuments, typeRef);
-        return results;
-    }
+		final List<Address> addressList = objectMapper
+			.readValue(jsonDocuments, new TypeReference<List<Address>>() {
+			});
+		return addressList;
+	}
 }
+
--- dao/src/main/java/com/postal/model/models/Address.java
+++ dao/src/main/java/com/postal/model/models/Address.java
@@ -1,26 +1,47 @@
 package com.postal.model.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Data
 @Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(Include.NON_NULL)
 public class Address {
 
-	String ID;
+	@JsonProperty
+	Map<String, String> _id;
+	@JsonProperty("ID")
+	String id;
+	@JsonProperty("LON")
 	String longitude;
+	@JsonProperty("LAT")
 	String latitude;
+	@JsonProperty("NUMBER")
 	String number;
+	@JsonProperty("STREET")
 	String street;
+	@JsonProperty("UNIT")
 	String unit;
+	@JsonProperty("CITY")
 	String city;
-	String postCode;
-	String country;
+	@JsonProperty("POSTCODE")
+	String postcode;
+	@JsonProperty("REGION")
+	String region;
+	@JsonProperty("DISTRICT")
+	String district;
+	@JsonProperty("HASH")
 	String hash;
 }
